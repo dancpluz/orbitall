e557ae848ba69d35d6565794cc170b917f55dde0
dancpluz <dan08jan@gmail.com>
2025-09-18 19:36:22 -0300

    feat: first get work

--- src/main/java/br/com/orbitall/hackathon2025/controllers/WelcomeController.java
+++ src/main/java/br/com/orbitall/hackathon2025/controllers/WelcomeController.java
@@ -1,9 +1,11 @@
 package br.com.orbitall.hackathon2025.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class WelcomeController {
 
     @GetMapping
