e468872ef493b250d334b981e87208cbcb7cbaf0
dancpluz <dan08jan@gmail.com>
2025-09-18 20:18:26 -0300

    feat: add repository and service

---
+++ src/main/java/br/com/orbitall/hackathon2025/services/PersonService.java
@@ -0,0 +1,26 @@
+package br.com.orbitall.hackathon2025.services;
+
+import br.com.orbitall.hackathon2025.models.Person;
+import br.com.orbitall.hackathon2025.repositories.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Service
+public class PersonService {
+    @Autowired
+    private PersonRepository repository;
+
+    public Person save(Person person) {
+        LocalDateTime now  = LocalDateTime.now();
+
+        person.setId(UUID.randomUUID());
+        person.setCreatedAt(now);
+        person.setUpdatedAt(now);
+        person.setActive(true);
+
+        return repository.save(person);
+    }
+}
