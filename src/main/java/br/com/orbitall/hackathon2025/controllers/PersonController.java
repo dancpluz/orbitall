c3f42455493577680c92629fe998ed6854652126
dancpluz <dan08jan@gmail.com>
2025-09-18 22:00:42 -0300

    fix: clean imports

--- src/main/java/br/com/orbitall/hackathon2025/controllers/PersonController.java
+++ src/main/java/br/com/orbitall/hackathon2025/controllers/PersonController.java
@@ -1,12 +1,10 @@
 package br.com.orbitall.hackathon2025.controllers;
 
 import br.com.orbitall.hackathon2025.models.Person;
-import br.com.orbitall.hackathon2025.repositories.PersonRepository;
 import br.com.orbitall.hackathon2025.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.util.UUID;
 
 @RestController
