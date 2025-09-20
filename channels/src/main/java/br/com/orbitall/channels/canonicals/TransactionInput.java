e9988d9bc4ada769685f694341f002085b807e02
dancpluz <dan08jan@gmail.com>
2025-09-20 13:26:40 -0300

    feat: add validation for credit and debit

--- channels/src/main/java/br/com/orbitall/channels/canonicals/TransactionInput.java
+++ channels/src/main/java/br/com/orbitall/channels/canonicals/TransactionInput.java
@@ -3,6 +3,7 @@ package br.com.orbitall.channels.canonicals;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Pattern;
 
 import java.math.BigDecimal;
 import java.util.UUID;
@@ -14,6 +15,7 @@ public record TransactionInput(
     @Positive(message = "Amount must be a positive value")
     BigDecimal amount,
     @NotBlank(message = "Card type cannot be blank")
+    @Pattern(regexp = "^(CRÉDITO|DÉBITO)$", message = "Card type must be 'CRÉDITO' or 'DÉBITO'")
     String cardType
 ){
 }
