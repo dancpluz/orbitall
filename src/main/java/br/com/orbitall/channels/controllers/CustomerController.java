a9af8eaf35ab57b0b0c787a6aa4973d185bb1947
dancpluz <dan08jan@gmail.com>
2025-09-20 11:02:47 -0300

    feat: controller and service skeleton

--- src/main/java/br/com/orbitall/channels/controllers/CustomerController.java
+++ src/main/java/br/com/orbitall/channels/controllers/CustomerController.java
@@ -1,9 +1,43 @@
 package br.com.orbitall.channels.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.orbitall.channels.canonicals.CustomerInput;
+import br.com.orbitall.channels.canonicals.CustomerOutput;
+import br.com.orbitall.channels.services.CustomerService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/customers")
 public class CustomerController {
+    @Autowired
+    private CustomerService service;
+
+    @PostMapping
+    public CustomerOutput createCustomer(@Valid @RequestBody CustomerInput input) {
+        return service.create(input);
+    }
+
+    @GetMapping("/{id}")
+    public CustomerOutput retrieve(@PathVariable UUID id) {
+        return service.retrieve(id);
+    }
+
+    @PutMapping("/{id}")
+    public CustomerOutput update(@PathVariable UUID id, @RequestBody CustomerInput input) {
+        return service.update(id, input);
+    }
+
+    @DeleteMapping("/{id}")
+    public CustomerOutput delete(@PathVariable UUID id) {
+        return service.delete(id);
+    }
+
+    @GetMapping
+    public List<CustomerOutput> findAll() {
+        return service.findAll();
+    }
 }
--- src/main/java/br/com/orbitall/channels/services/CustomerService.java
+++ src/main/java/br/com/orbitall/channels/services/CustomerService.java
@@ -1,4 +1,41 @@
 package br.com.orbitall.channels.services;
 
+import br.com.orbitall.channels.canonicals.CustomerInput;
+import br.com.orbitall.channels.canonicals.CustomerOutput;
+import br.com.orbitall.channels.models.Customer;
+import br.com.orbitall.channels.repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Service
 public class CustomerService {
+    @Autowired
+    private CustomerRepository repository;
+
+    public CustomerOutput create(CustomerInput input) {
+
+    }
+
+    public CustomerOutput retrieve(UUID id) {
+
+    }
+
+    public CustomerOutput update(UUID id, CustomerInput input) {
+
+    }
+
+    public CustomerOutput delete(UUID id) {
+
+    }
+
+    public List<CustomerOutput> findAll() {
+
+    }
+
 }
+
