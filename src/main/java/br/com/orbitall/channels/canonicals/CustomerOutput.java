aa3a01f566581d5c25be44323fd119ac54537879
dancpluz <dan08jan@gmail.com>
2025-09-20 11:14:29 -0300

    feat: create and retrieve customers

--- src/main/java/br/com/orbitall/channels/canonicals/CustomerOutput.java
+++ src/main/java/br/com/orbitall/channels/canonicals/CustomerOutput.java
@@ -1,8 +1,10 @@
 package br.com.orbitall.channels.canonicals;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 public record CustomerOutput(
+        UUID id,
         String fullName,
         String email,
         String phone,
--- src/main/java/br/com/orbitall/channels/services/CustomerService.java
+++ src/main/java/br/com/orbitall/channels/services/CustomerService.java
@@ -18,11 +18,46 @@ public class CustomerService {
     private CustomerRepository repository;
 
     public CustomerOutput create(CustomerInput input) {
+        LocalDateTime now = LocalDateTime.now();
 
+        Customer customer = new Customer();
+
+        customer.setId(UUID.randomUUID());
+        customer.setFullName(input.fullName());
+        customer.setEmail(input.email());
+        customer.setPhone(input.phone());
+        customer.setCreatedAt(now);
+        customer.setUpdatedAt(now);
+        customer.setActive(true);
+
+        repository.save(customer);
+
+        return new CustomerOutput(
+                customer.getId(),
+                customer.getFullName(),
+                customer.getEmail(),
+                customer.getPhone(),
+                customer.getCreatedAt(),
+                customer.getUpdatedAt(),
+                customer.isActive()
+        );
     }
 
     public CustomerOutput retrieve(UUID id) {
+        Customer customer = repository
+                .findById(id)
+                .filter(Customer::isActive)
+                .orElseThrow(() -> new ResourceNotFoundException("Not found the resource (id: " + id + ")"));
 
+        return new CustomerOutput(
+                customer.getId(),
+                customer.getFullName(),
+                customer.getEmail(),
+                customer.getPhone(),
+                customer.getCreatedAt(),
+                customer.getUpdatedAt(),
+                customer.isActive()
+        );
     }
 
     public CustomerOutput update(UUID id, CustomerInput input) {
